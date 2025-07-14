8899daa17c753e1da3b75317af3f5fca2357a5df
Divya <divyadarsheel.dds@gmail.com>
2025-07-14 23:09:23 +0545

    Updated base for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'mithila-magic-pickle-shop',
+  base: '/mithila-magic-pickle-shop',
   server: {
     host: "::",
     port: 8080,
